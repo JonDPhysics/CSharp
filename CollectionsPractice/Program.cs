d720968f3d38b5cca5f1fa5c1fae30ecbe2918c2
Jonathan Smith <jonathan.smith722@gmail.com>
2021-11-30 12:06:02 -0800

    Updated 11/30/21

--- CollectionsPractice/Program.cs
+++ CollectionsPractice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CollectionsPractice
 {
@@ -7,20 +8,33 @@ namespace CollectionsPractice
         static void Main(string[] args)
         {
             // Three Basic Arrays
-            int arrayI = new int[10];
-            string arrayS = new string[] {"Tim", "Martin", "Nikki", "Sara"};
+            int[] arrayI = new int[10];
+            string[] arrayS = new string[] {"Tim", "Martin", "Nikki", "Sara"};
             bool[] arrayB = new bool[] {true, false, true, false, true, false, true, false, true, false};
 
             // List of Flavors
-            List<string> icecream = new List<string>("chocolate", "vanilla", "cookies and cream", "strawberry", "huckleberry");
-            Console.WriteLine(icecream.Length);
+            List<string> icecream = new List<string>();
+            icecream.Add("chocolate");
+            icecream.Add("vanilla");
+            icecream.Add("cookies and cream");
+            icecream.Add("strawberry");
+            icecream.Add("huckleberry");
+            Console.WriteLine(icecream.Count);
             Console.WriteLine(icecream[2]);
-            icecream.Remove[2];
-            Console.WriteLine(icecream.Length);
+            icecream.RemoveAt(2);
+            Console.WriteLine(icecream.Count);
 
             // User Info Dictionary
             Dictionary<string,string> nameFlavor = new Dictionary<string, string>();
-            
+            Random rnd = new Random();
+            nameFlavor.Add(arrayS[0], icecream[rnd.Next(icecream.Count)]);
+            nameFlavor.Add(arrayS[1], icecream[rnd.Next(icecream.Count)]);
+            nameFlavor.Add(arrayS[2], icecream[rnd.Next(icecream.Count)]);
+            nameFlavor.Add(arrayS[3], icecream[rnd.Next(icecream.Count)]);
+            foreach (var entry in nameFlavor)
+            {
+                Console.WriteLine(entry.Key + ": " + entry.Value);
+            }
         }
     }
 }
