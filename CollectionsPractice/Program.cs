53c37da6a411cd16d0aa73b0802ddc5e6678cf25
Jonathan Smith <jonathan.smith722@gmail.com>
2021-11-30 09:06:32 -0800

    Updated 11/30/21

---
+++ CollectionsPractice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CollectionsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Three Basic Arrays
+            int arrayI = new int[10];
+            string arrayS = new string[] {"Tim", "Martin", "Nikki", "Sara"};
+            bool[] arrayB = new bool[] {true, false, true, false, true, false, true, false, true, false};
+
+            // List of Flavors
+            List<string> icecream = new List<string>("chocolate", "vanilla", "cookies and cream", "strawberry", "huckleberry");
+            Console.WriteLine(icecream.Length);
+            Console.WriteLine(icecream[2]);
+            icecream.Remove[2];
+            Console.WriteLine(icecream.Length);
+
+            // User Info Dictionary
+            Dictionary<string,string> nameFlavor = new Dictionary<string, string>();
+            
+        }
+    }
+}
