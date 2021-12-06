3300225d7f5fc6cd440eee1fbdc40dd79c09147f
Jonathan Smith <jonathan.smith722@gmail.com>
2021-12-06 09:56:18 -0800

    Updated 12/06/21

---
+++ portfolio2/Controllers/IndexControllers.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+namespace portfolio2.Controllers
+{
+    public class IndexController : Controller
+    {
+        [HttpGet("/")]
+        public ViewResult Index()
+        {
+            return View();
+        }
+        [HttpGet("projects")]
+        public ViewResult projects()
+        {
+            return View();
+        }
+        [HttpGet("contact")]
+        public ViewResult contact()
+        {
+            return View();
+        }
+        [HttpPost("home")]
+        public ViewResult home()
+        {
+            return View();
+        }
+    }
+}
