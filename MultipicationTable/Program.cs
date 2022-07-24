48ae831a7d2b499b96d1afc5519ae7152842d378
Jonathan Smith <jonathan.smith722@gmail.com>
2022-07-24 09:10:07 -0600

    updated 20220724

--- MultipicationTable/Program.cs
+++ MultipicationTable/Program.cs
@@ -6,7 +6,10 @@ namespace MultipicationTable
     {
         static void Main(string[] args)
         {
-            
+            int [,,,,,,,,,] ToTensTable = new int[1,2,3,4,5,6,7,8,9,10]
+            {
+                
+            }
         }
     }
 }
